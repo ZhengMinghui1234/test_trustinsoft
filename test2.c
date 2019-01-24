eed838f55029d5bf3b9d38711561c92098cb30a0
root <982079709@qq.com>
2019-01-24 15:41:15 +0800

    test2.c

---
+++ test2.c
@@ -0,0 +1,20 @@
+int main(void)
+{
+  int array[5] = {13, 7, 42, 0, 1};
+  int i, n;
+  int a = 10;
+  for (i = 0; i < 5; i++) {
+    /* The variable 'n' is declared at line 4, but is never
+       initialized before usage. The analyzer emit then an alarm
+       about uninitialized variable.
+       This is an undefined behavior to read a local variable
+       that has not been initialized. */
+    n += array[i];
+  }
+  if(a > 0) {
+    a += a;
+  } else {
+    a *= a;
+  }
+  return 0;
+}
